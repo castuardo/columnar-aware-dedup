479bb4093c141a92c0047ae6362e2a958d32dd36
castuardo <castuardo@uchicago.edu>
2020-07-09 13:02:12 -0500

    basic structure refactor

--- scripts/testmain/environment.sh
+++ scripts/testmain/environment.sh
@@ -3,8 +3,8 @@
 #############
 ## add some general properties here
 #############
-export BASE_DIR=".."
+export BASE_DIR="../.."
 export MAIN_JAR="$BASE_DIR/ColumnarAwareDedup.jar"
 export CLASS_PATH="$BASE_DIR/deps/*"
-export LOG_PROPS="-Dlog4j.configurationFile=$BASE_DIR/conf/log4j.xml"
+export LOG_PROPS="-Dlog4j.configurationFile=$BASE_DIR/conf/log4j-default.xml"
 
--- scripts/testmain/runTestMain.sh
+++ scripts/testmain/runTestMain.sh
@@ -2,10 +2,8 @@
 ## source the env first
 #########
 source environment.sh
-
 ## This is the main class here, no params...
 MAIN="vmware.speedup.cawd.main.TestMain"
 ## now run it
-
 java -cp $MAIN_JAR:$CLASS_PATH $LOG_PROPS $MAIN
 
