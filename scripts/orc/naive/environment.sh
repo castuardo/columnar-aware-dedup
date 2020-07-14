f4cc611dfb9b5eaf0f25da9f8d6871e42b18c0cb
castuardo@uchicago.edu <castuardo@uchicago.edu>
2020-07-14 10:17:49 -0700

    partial before lunch

--- scripts/orc/naive/environment.sh
+++ scripts/orc/naive/environment.sh
@@ -3,7 +3,7 @@
 #############
 ## add some general properties here
 #############
-export BASE_DIR="../.."
+export BASE_DIR="../../.."
 export MAIN_JAR="$BASE_DIR/ColumnarAwareDedup.jar"
 export CLASS_PATH="$BASE_DIR/deps/*"
 
