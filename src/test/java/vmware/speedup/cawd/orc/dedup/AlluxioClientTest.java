ef9116698c9f0bbb817cdcac99e314d922b7d232
Ubuntu <ubuntu@ip-172-31-26-55.us-east-2.compute.internal>
2020-08-11 03:40:34 +0000

    new testing

--- src/test/java/vmware/speedup/cawd/orc/dedup/AlluxioClientTest.java
+++ src/test/java/vmware/speedup/cawd/orc/dedup/AlluxioClientTest.java
@@ -9,6 +9,7 @@ import alluxio.client.file.FileOutStream;
 import alluxio.client.file.FileSystem;
 import alluxio.conf.AlluxioConfiguration;
 import alluxio.conf.InstancedConfiguration;
+import alluxio.conf.PropertyKey;
 
 public class AlluxioClientTest {
 
@@ -17,10 +18,16 @@ private static final Logger logger = LogManager.getLogger(AlluxioClientTest.clas
 
     @Test
     public void doTest() {
-    	AlluxioURI uri = new AlluxioURI("");
-    	FileOutStream writer = getWriter(uri, InstancedConfiguration.defaults());
+	String file = "alluxio://ip-172-31-26-162.us-east-2.compute.internal:19998/other/test-1717-7.orc";
+	logger.info("Trying out {}", file);
+    	AlluxioURI uri = new AlluxioURI(file);
+        InstancedConfiguration conf = InstancedConfiguration.defaults();
+	conf.set(PropertyKey.MASTER_HOSTNAME, "ip-172-31-26-162.us-east-2.compute.internal");
+    	conf.set(PropertyKey.USER_FILE_WRITE_TYPE_DEFAULT, "ASYNC_THROUGH");
+        FileOutStream writer = getWriter(uri, conf);
     	if(writer != null) {
     		try {
+			logger.info("Got connected, writing..");
     			// do something here
     			writer.write("Hello, this is working!".getBytes());
     		}
@@ -28,9 +35,13 @@ private static final Logger logger = LogManager.getLogger(AlluxioClientTest.clas
     			logger.error("Error while writing...", e);
     		}
     		finally {
+			logger.info("done!");
     			closeWriter(writer);
     		}
     	}
+	else {
+		logger.info("Error, never connected...");
+	}
     	
     }
    
